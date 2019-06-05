19dac5be78a40c5ca922015f4057f97ebd97e8d7
Magdalena Ambrozy <magda.ambrozy@gmail.com>
2019-06-05 18:26:37 +0200

    Add table to gameOverModal

--- js/script.js
+++ js/script.js
@@ -1,44 +1,109 @@
 'use strict'
-window.onload = function() {
-  reset();
-};
 
-var btnPaper = document.getElementById('paper');
-var btnStone = document.getElementById('stone');
-var btnScissors = document.getElementById('scissors');
+// window.onload = function() {
+//   reset();
+// };
+var params = {
+  //playerName : '',
+  totalRounds  : 0,
+  roundsPlayed : 0,
+  p1RoundScore : 0,
+  p2RoundScore : 0,
+  p1Score      : 0,
+  p2Score      : 0,
+  p1Move       : null,
+  p2Move       : null,
+  progress     : []
+}
+var buttons = Array.from(document.querySelectorAll('.player-move'));
 var btnReset = document.getElementById('reset');
 var output = document.getElementById('output');
 var result = document.getElementById('result');
-var gameOver= document.getElementById('gameOver');
-var totalRounds;
 
-var player1;
-var player2;
-var p1Score;
-var p2Score;
+//modal //
+
+var newGameModal = document.getElementById('newGameModal');
+
+//var startBtn = document.getElementById('startBtn');
+var gameOverModal = document.getElementById('gameOverModal');
+//var newGameModal = document.getElementById('newGameModal');
+var progressTable = document.getElementById('progressTable');
+
+var modals = document.querySelectorAll('.modal');
+
+for(var i = 0; i < modals.length; i++){
+  modals[i].addEventListener('click', function(event){
+    event.stopPropagation();
+  });
+}
+
+var showModal = function(event, modal, content){
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.add('show');
+      
+  for(var i = 0; i < modals.length; i++){
+  modals[i].classList.remove('show');
+  }
+ 
+  modal.classList.add('show');
+  if(content){
+    modal.querySelector('.content').textContent = content;
+  }
+  
+};
+
+// var modalLinks = document.querySelectorAll('.show-modal');
+// for(var i = 0; i < modalLinks.length; i++){
+//   modalLinks[i].addEventListener('click', showModal);
+// } 
+var hideModal = function(event){
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.remove('show');
+};
+var closeButtons = document.querySelectorAll('.modal .close');
+for(var i = 0; i < closeButtons.length; i++){
+  closeButtons[i].addEventListener('click', hideModal);
+}
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+
+
+
 
 function reset(){
-  gameOver.innerHTML='';
-  p1Score = 0;
-  p2Score = 0;
-  btnPaper.disabled = false;
-  btnStone.disabled = false;
-  btnScissors.disabled = false;
+  //gameOver.innerHTML='';
+  params.playerName = '';
+  params.p1Score = 0;
+  params.p2Score = 0;
+  params.p1RoundScore = 0;
+  params.p2RoundScore = 0;
+  params.roundsPlayed = 0;
+  params.p1Move = null;
+  params.p2Move = null;
+  params.progress = [];
+  buttons.forEach(function (item) {
+    item.disabled = false;
+  });
+  var tableParams = Array.from(document.querySelectorAll('.tableParams'));
+  if (tableParams){
+    for(i = 0; i < tableParams.length; i++){
+      tableParams[i].classList.add('hide');
+    }
+  }
   log('');
-  result.innerHTML= p1Score + ':' + p2Score;
-  totalRounds = window.prompt('How many rounds do you wish to play?');
+  result.innerHTML= params.p1Score + ':' + params.p2Score;
+  params.totalRounds = window.prompt('How many rounds do you wish to play?');
 };
 
 function move2(){
     var num = Math.round(Math.random()*2+1);
     if (num===1){
-      player2='paper';
+      params.p2Move = 'paper';
     }
     if (num===2){
-      player2='stone';
+      params.p2Move = 'stone';
     }
     if (num===3){
-      player2='scissors';
+      params.p2Move = 'scissors';
     }
   };
 
@@ -49,66 +114,111 @@ function log(text){
   };
 
 var playerMove= function(event){
+  params.roundsPlayed++;
+  
+  console.log(params.roundsPlayed);
 
-if (this.id=='paper') {
-  player1='paper';
-}
-if (this.id== 'stone') {
-  player1 ='stone';
-}
-if (this.id=='scissors') {
-  player1='scissors';
-}
-console.log(player1);
+  params.p1Move = this.getAttribute('data-move');
+
+console.log(params.p1Move);
 move2();
-console.log(player2);
+console.log(params.p2Move);
 
-  if (player1==player2){
+  if (params.p1Move==params.p2Move){
     log('Both Players draw the same!');
+    params.p1RoundScore = 0;
+    params.p2RoundScore = 0;
   }
-  else if (player1=='paper' && player2=='stone'){
+  else if (params.p1Move == 'paper' && params.p2Move == 'stone'){
     log('YOU WON! you played PAPER, computer played ROCK');
-    p1Score++;
+    params.p1Score++;
+    params.p1RoundScore = 1;
+    params.p2RoundScore = 0;
   }
-  else if (player1=='stone' && player2=='scissors'){
+  else if (params.p1Move == 'stone' && params.p2Move == 'scissors'){
     log('YOU WON: you played ROCK, computer played SCISSORS');
-    p1Score++;
+    params.p1Score++;
+    params.p1RoundScore = 1;
+    params.p2RoundScore = 0;
   }
-  else if (player1=='scissors' && player2=='paper'){
+  else if (params.p1Move == 'scissors' && params.p2Move == 'paper'){
     log('YOU WON: you played scissors, computer played paper');
-    p1Score++;
+    params.p1Score++;
+    params.p1RoundScore = 1;
+    params.p2RoundScore = 0;
   }
-  else if (player2=='paper' && player1=='stone'){
+  else if (params.p2Move == 'paper' && params.p1Move == 'stone'){
     log('COMPUTER WON: you played stone, computer played paper');
-    p2Score++;
+    params.p2Score++;
+    params.p1RoundScore = 0;
+    params.p2RoundScore = 1;
     
   }
-  else if (player2=='stone' && player1=='scissors'){
+  else if (params.p2Move =='stone' && params.p1Move =='scissors'){
     log('COMPUTER WON: you played scissors, computer played paper');
-    p2Score++;
+    params.p2Score++;
+    params.p1RoundScore = 0;
+    params.p2RoundScore = 1;
   }
-  else if (player2=='scissors' && player1=='paper'){
+  else if (params.p2Move =='scissors' && params.p1Move == 'paper'){
     log('COMPUTER WON: you played paper, computer played scissors');
-    p2Score++;  
+    params.p2Score++;
+    params.p1RoundScore = 0;
+    params.p2RoundScore = 1; 
   };
-  result.innerHTML= p1Score + ':' + p2Score;
+  result.innerHTML= `${params.p1Score} : ${params.p2Score}`;
 
-  if (p1Score==totalRounds || p2Score==totalRounds) {
-    btnPaper.disabled = true;
-    btnStone.disabled = true;
-    btnScissors.disabled = true;
-    if (p1Score > p2Score){
-      gameOver.innerHTML='YOU WON THE GAME!!!';
+  params.progress.push({
+    tRoundsPlayed : params.roundsPlayed,
+    tPlayer1Move  : params.p1Move,
+    tPlayer2Move  : params.p2Move,
+    tRoundScore   : `${params.p1RoundScore} : ${params.p2RoundScore}`,
+    tTotalScore   :`${params.p1Score} : ${params.p2Score}`
+  })
+  console.log(params.progress);
+  var index = params.roundsPlayed - 1;
+  var tableRow = `
+  <tr class='tableParams'>
+    <td>${params.progress[index].tRoundsPlayed}</td>
+    <td>${params.progress[index].tPlayer1Move}</td>
+    <td>${params.progress[index].tPlayer2Move}</td>
+    <td>${params.progress[index].tRoundScore}</td>
+    <td>${params.progress[index].tTotalScore}</td>
+</tr>
+  `;
+  progressTable.insertAdjacentHTML("afterend", tableRow);
+
+  if (params.p1Score == params.totalRounds || params.p2Score==params.totalRounds) {
+    buttons.forEach(function (item) {
+      item.disabled = true;
+    });
+    var modalContent;
+    if (params.p1Score > params.p2Score){
+      //gameOver.innerHTML='YOU WON THE GAME!!!';
+      modalContent = 'YOU WON THE GAME!!!';
+
     }
     else {
-      gameOver.innerHTML='COMPUTER WON THE GAME!!!';
-    }
-    log("Game over, please press the new game button!"); 
+      //gameOver.innerHTML='COMPUTER WON THE GAME!!!';
+      modalContent = 'COMPUTER WON THE GAME!!!';
+    } 
+    //log("Game over, please press the new game button!");
+    showModal(event, gameOverModal, modalContent);
   };
+  
 };
 
+buttons.forEach(function (item) {
+  item.addEventListener('click', playerMove);
+});
 
-btnPaper.addEventListener('click', playerMove);
-btnStone.addEventListener('click', playerMove);
-btnScissors.addEventListener('click', playerMove);
-btnReset.addEventListener('click', reset); 
+//var playerName = document.getElementById('name').value;
+//console.log(playerName);
+// function startGame(){
+//   reset();
+//   params.totalRounds = document.getElementById('rounds').value;
+//   playerName = document.getElementById('name').value;
+// }
+btnReset.addEventListener('click', reset);
+ //btnReset.addEventListener('click', showModal(newGameModal));
+//startBtn.addEventListener('click', startGame); 
